7ff5b95d498921903d655a17f4371274d64840f9
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-09 19:18:49 +0530

    feat [ stage 6 ] : Add Animation Component

---
+++ src/utils/animations.js
@@ -0,0 +1,26 @@
+export const animateWithGsapTimeline = (
+    timeLine, rotationRef, rotationState, firstTarget, secondTarget, animationProps
+) => {
+
+    timeLine.to(rotationRef.current.rotation, {
+        y: rotationState,
+        duration: 1,
+        ease: 'power2.inOut'
+    })
+
+    timeLine.to(firstTarget,
+        {
+            ...animationProps,
+            ease: 'power2.inOut'
+        },
+        '<'
+    )
+
+    timeLine.to(secondTarget,
+        {
+            ...animationProps,
+            ease: 'power2.inOut'
+        },
+        '<'
+    )
+}
