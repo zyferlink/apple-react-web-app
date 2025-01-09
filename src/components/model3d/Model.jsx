ac0422a12dae206592f52b97e22394a02242718a
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-09 19:19:20 +0530

    feat [ stage 6 ] : Apply Animation Component

--- src/components/model3d/Model.jsx
+++ src/components/model3d/Model.jsx
@@ -1,5 +1,5 @@
 import { useGSAP } from '@gsap/react'
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import gsap from 'gsap'
 import ModelView from './ModelView'
 import { yellowImg } from '../../utils'
@@ -7,6 +7,7 @@ import * as THREE from 'three'
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
 import { models, sizes } from '../../constants/index'
+import { animateWithGsapTimeline } from '../../utils/animations'
 
 const Model = () => {
 
@@ -29,6 +30,27 @@ const Model = () => {
     // rotation
     const [smallRotation, setSmallRotation] = useState(0);
     const [largeRotation, setLargeRotation] = useState(0);
+
+    const timeLine = gsap.timeline();
+
+    useEffect(() => {
+        if(size === 'large'){
+            animateWithGsapTimeline(
+                timeLine, small, smallRotation, "#view1", "#view2", {
+                transform: 'translateX(-100%)',
+                duration: 2
+            })
+        }
+
+        if(size === 'small'){
+            animateWithGsapTimeline(
+                timeLine, large, largeRotation, "#view2", "#view1", {
+                transform: 'translateX(0)',
+                duration: 2
+            })
+        }
+
+    }, [size])
 
     useGSAP(() => {
         gsap.to('#heading', { y: 0, opacity: 1 })
