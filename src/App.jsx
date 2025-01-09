aff87df871419a9029fe4e08c22092b7f9369f6d
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-09 21:21:38 +0530

    feat [ stage 7 ] : Apply Feature component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Model from "./components/model3d/Model"
+import Features from "./components/Features"
 
 const App = () => {
 
@@ -11,6 +12,7 @@ const App = () => {
     <Hero/>
     <Highlights/>
     <Model/>
+    <Features/>
     </main>
   )
 }
