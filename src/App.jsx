cf567def89f5a4a2f5e3c2ae1c1aa9af96fda2d8
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-10 18:58:30 +0530

    feat [ loading ] : onStart loading Animation | v3

--- src/App.jsx
+++ src/App.jsx
@@ -5,42 +5,29 @@ import Model from "./components/iphonemodel/Model"
 import Features from "./components/Features"
 import HowItWorks from "./components/HowItWorks"
 import Footer from "./components/Footer"
-import { appleImg } from "./utils";
+import AppLoader from "./components/loader/AppLoader"
 
 import { useState, useEffect } from "react";
 
+
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
+  const [fadeOut, setFadeOut] = useState(false);
 
   useEffect(() => {
-    // Simulate a loading time (e.g., fetching data)
     const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 1000); // 2 seconds
-
-    // Cleanup the timer
+      setFadeOut(true); 
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 600); 
+    }); 
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <main className="bg-black">
       {isLoading ? (
-        <div className="flex items-center justify-center h-screen bg-black flex-col">
-          {/* Logo */}
-          <img
-            src={appleImg}
-            alt="Apple Logo"
-            width={32}
-            height={26}
-          />
-
-          {/* Loader container */}
-          <div className="loader-container mt-8">
-            <div className="line-loader">
-              <div className="line"></div>
-            </div>
-          </div>
-        </div>
+        <AppLoader fadeOut={fadeOut} />
       ) : (
         <>
           <Navbar />
