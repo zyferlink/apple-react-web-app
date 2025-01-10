61785a0dcf619b9a4b84a38e56f6f3c985d6864d
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-10 18:21:32 +0530

    feat [ loading ] : onStart loading Animation | v1

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,60 @@ import Model from "./components/iphonemodel/Model"
 import Features from "./components/Features"
 import HowItWorks from "./components/HowItWorks"
 import Footer from "./components/Footer"
+import { appleImg } from "./utils";
+
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a loading time (e.g., fetching data)
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // 2 seconds
+
+    // Cleanup the timer
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center h-screen bg-black flex-col">
+
+        {/* Logo */}
+        <img
+          src={appleImg}
+          alt="Apple Logo"
+          width={32}
+          height={26}
+        />
+
+        {/* Loader container */}
+        <div className="loader-container mt-8"> 
+          <div className="line-loader">
+            <div className="line"></div>
+          </div>
+        </div>
+
+      </div>
+
+
+    );
+  }
 
   return (
-    <main className="bg-black"> 
-    <Navbar/>
-    <Hero/>
-    <Highlights/>
-    <Model/>
-    <Features/>
-    <HowItWorks/>
-    <Footer/>
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
