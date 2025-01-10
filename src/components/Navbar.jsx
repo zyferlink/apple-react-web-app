f67d4fb8f06b913339c61ddcc985043bcf690739
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-10 15:29:30 +0530

    refactor [ Navbar ] : Improve readability (components/Navbar.jsx)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,37 @@
-import { appleImg, bagImg, searchImg } from '../utils'
-import { navLists } from '../constants'
+import { appleImg, bagImg, searchImg } from "../utils";
+import { navLists } from "../constants";
 
 const Navbar = () => {
+
+    // Render navigation items
+    const renderNavItems = () =>
+        navLists.map((nav) => (
+            <li
+                key={nav}
+                className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all" >
+                {nav}
+            </li>
+        ));
+
     return (
-        <header
-            className='w-full py-5 sm:px-10 px-5 flex justify-between items-center'>
-            <nav
-                className='flex w-full screen-max-width'>
-                <img src={appleImg} alt="apple" width={14} height={18} />
+        <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+            <nav className="flex w-full screen-max-width items-center">
+                {/* Logo */}
+                <img src={appleImg} alt="Apple Logo" width={14} height={18} />
 
-                <div
-                    className='flex flex-1 justify-center max-sm:hidden'>
-                    {
-                        navLists.map(
-                            (nav) => (
-                                <div
-                                    key={nav}
-                                    className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all">
-                                    {nav}
-                                </div>
-                            ))
-                    }
+                {/* Navigation List */}
+                <ul className="flex flex-1 justify-center max-sm:hidden">
+                    {renderNavItems()}
+                </ul>
+
+                {/* Utility Icons */}
+                <div className="flex items-center gap-7 max-sm:justify-end max-sm:flex-1">
+                    <img src={searchImg} alt="Search Icon" width={18} height={18} />
+                    <img src={bagImg} alt="Bag Icon" width={18} height={18} />
                 </div>
-
-                <div
-                className='flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1'>
-                    <img src={searchImg} alt="search" width={18} height={18} />
-
-                    <img src={bagImg} alt="bag" width={18} height={18} />
-                </div>
-
             </nav>
         </header>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
