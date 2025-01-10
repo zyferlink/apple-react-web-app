a953aaf6dae2ec9eed98a09faa93b49a5c3d1cc0
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-10 17:18:34 +0530

    refactor [ Loader ] : Improve readability (components/loader/Loader.jsx) v1

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,16 +1,19 @@
-import { Html } from '@react-three/drei'
-import React from 'react'
+import { Html } from '@react-three/drei';
+import React from 'react';
+import './Loader.css'; 
 
 const Loader = () => {
     return (
         <Html
             className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
-            <div 
-            className="w-[10vm] h-[10vm] rounded-full">
-                Loading...
+            <div className="loader-container">
+                <div className="line-loader">
+                    <div className="line"></div>
+                </div>
+                <span className="loading-text">Loading...</span>
             </div>
         </Html>
-    )
-}
+    );
+};
 
-export default Loader
+export default Loader;
